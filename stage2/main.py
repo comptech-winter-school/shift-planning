63d2d1fa9009ec395a4b4d65140dd18c85fce1c2
Orlan Sar <60601286+OrlanSar@users.noreply.github.com>
2022-02-05 19:54:25 +0700

    Add files via upload

---
+++ stage2/main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import datetime
+from stage2.models.model import S2Regressor
+from stage2.models.pipeline import *
+
+def get_optimal_partners(path_orders: str, path_partners_delays: str, pred_orders: pd.DataFrame) -> pd.DataFrame:
+    df_orders, df_partners_delays = get_data(path_orders, path_partners_delays)
+    df = get_features(df_orders, df_partners_delays=df_partners_delays)
+    X_train, y_train = get_train_test_split(df)
+    
+    model = S2Regressor('LR')
+    model.fit(X_train, y_train)
+    
+    partners_pred = optimal_partners(pred_orders, model)
+    
+    return partners_pred
